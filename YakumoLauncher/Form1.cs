63a53386293acc3c8a80ed98f1fc709034cc2a97
emakyy110s@outlook.jp <emakyy110s@outlook.jp>
2020-04-18 00:38:01 +0900

    プルリク用のテストコミット

--- YakumoLauncher/Form1.cs
+++ YakumoLauncher/Form1.cs
@@ -8,18 +8,49 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace YakumoLauncher
 {
     public partial class Form1 : Form
     {
+        //マウスのクリック位置を記憶
+        private Point mousePoint;
+
         public Form1()
         {
             InitializeComponent();
         }
-
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            pictureBox1.MouseMove += new MouseEventHandler(Form1_MouseMove);
         }
+
+        //Form1のMouseDownイベントハンドラ
+        //マウスのボタンが押されたとき
+        private void Form1_MouseDown(object sender,
+            System.Windows.Forms.MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                //位置を記憶する
+                mousePoint = new Point(e.X, e.Y);
+            }
+        }
+
+        //Form1のMouseMoveイベントハンドラ
+        //マウスが動いたとき
+        private void Form1_MouseMove(object sender,
+            System.Windows.Forms.MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                this.Location = new Point(
+                   this.Location.X + e.X - mousePoint.X,
+                   this.Location.Y + e.Y - mousePoint.Y);
+            }
+        }
+
+
     }
 }
